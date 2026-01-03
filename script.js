19e121d9d0a8eeb1fb216d9b7fff0f81d34fd420
Yotsakorn Klahan <Dracoosk@gmail.com>
2026-01-03 14:35:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// External script file
+// All main functionality is already in index.html
+// This file is kept for reference or additional features
+
+// Note: Removed duplicate code that was moved to index.html
+
